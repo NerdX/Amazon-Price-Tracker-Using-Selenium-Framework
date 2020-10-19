7ad06527f0cc08317c2e56c7d09786fec66c9b60
NerdX <nerdx01salekar@gmail.com>
2020-10-19 10:17:00 +0530

    first commit

---
+++ amazon_configuration.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+
+# GLOBAL - STATIC VARIABLES:
+DIRECTORY = "C:\\Users\\Anjaniy\\PycharmProjects\\AmazonPriceTracker\\reports"
+NAME = "playstation 4 pro"
+CURRENCY = '€'
+MIN_PRICE = '300'
+MAX_PRICE = '650'
+FILTERS = {
+    'min': MIN_PRICE,
+    'max': MAX_PRICE
+}
+# BASE_URL:
+BASE_URL = 'http://www.amazon.de/'
+
+
+# GET THE CHROME_DRIVER:
+def get_chrome_web_driver(options):
+    return webdriver.Chrome('./chromedriver', chrome_options=options)
+
+
+#  GET THE CHROME_OPTIONS:
+def get_chrome_options():
+    return webdriver.ChromeOptions()
+
+
+# FUNCTION FOR IGNORE THE CERTIFICATE ERRORS:
+def set_ignore_certificate_error(options):
+    options.add_argument('--ignore-certificate-errors')
+
+
+# FUNCTION FOR INCOGNITO_CHROME:
+def set_browser_as_incognito(options):
+    options.add_argument('--incognito')
+
+
+def set_automation_as_head_less(options):
+    options.add_argument('--headless')
